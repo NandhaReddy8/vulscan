cb91b298635960beb3b03861939a260583787e92
Edith <edith@Edith>
2025-03-28 06:17:32 -0400

    num error solved

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -53,8 +53,8 @@ function updateResults(results) {
         low: document.querySelector('.stat-card.low .count')
     };
 
-    animateCount(counters.high, results.high);
-    animateCount(counters.medium, results.medium);
+    animateCount(counters.high, results.high.length);
+    animateCount(counters.medium, results.medium.length);
     animateCount(counters.low, results.low.length);
 
     // Update low vulnerability list
